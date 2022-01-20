6dc377b28b196b11abf74ef37983a6994f4e9d09
Bramalamm <fredrik.bramell@gmail.com>
2022-01-20 19:41:41 +0100

    Update crawler.py

--- classes/crawler.py
+++ classes/crawler.py
@@ -1,3 +1,14 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
+''' -- BeautifulSoup -- '''
 url = "https://www.google.com/"
 soup = BeautifulSoup(url, 'html.parser')
+
+
+''' -- Selenium WebDriver -- '''
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+driver = webdriver.Chrome(executable_path="path/to/chromedriver.exe", chrome_options=options)
+driver.get("example.html")
+# do something here...
+driver.close()
