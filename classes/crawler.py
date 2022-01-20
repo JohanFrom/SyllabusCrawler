5bcd9ddbd170c36d719b248a34bc0f5094ae394c
Bramalamm <fredrik.bramell@gmail.com>
2022-01-20 20:17:31 +0100

    Testade selenium

--- classes/crawler.py
+++ classes/crawler.py
@@ -1,14 +1,28 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
+import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+
 ''' -- BeautifulSoup -- '''
-url = "https://www.google.com/"
-soup = BeautifulSoup(url, 'html.parser')
+#url = "https://www.google.com/"
+#soup = BeautifulSoup(url, 'html.parser')
 
 
 ''' -- Selenium WebDriver -- '''
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-driver = webdriver.Chrome(executable_path="path/to/chromedriver.exe", chrome_options=options)
-driver.get("example.html")
-# do something here...
-driver.close()
+s=Service(ChromeDriverManager().install())
+PATH = "C:\Program Files (x86)\chromedriver.exe"
+driver = webdriver.Chrome(PATH, service=s)
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True) 
+driver.maximize_window()
+
+
+driver.get("https://www.wikipedia.com") # Startar browsern - OBS använd försiktigt
+
+
+while True: # While loop som håller fönstret öppet
+    print(".")
+    time.sleep(5)
+    # Avbryt loopen med "Ctrl + C" i terminalen (temporär lösning)
