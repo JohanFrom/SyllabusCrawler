fc5899ce619b7dbd3c3e2f873892b90083fb4df1
JohanFrom <gouldsilver@gmail.com>
2022-01-24 15:18:04 +0100

    Created simple GUI

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
-# Här är main koden körs härifrån
+# Here runs the main code
+from flask import Flask, render_template
+
+app = Flask(__name__, static_url_path='/static')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.secret_key = "SecretKey"
+    app.run(debug=True)
