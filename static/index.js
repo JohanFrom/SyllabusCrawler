e4ec01f5b51459ce8ada8f9e55cc13c9dffb0f91
Bramalamm <fredrik.bramell@gmail.com>
2022-02-24 16:56:34 +0100

    test javascript

--- static/index.js
+++ static/index.js
@@ -1,6 +1,11 @@
 CheckInput = () => {
     const form = document.forms["search_form"]["search-input"].value
     const numbersInput = document.forms["search_form"]["amount-of-pages"].value
+
+    const keyword1 = document.forms["search_form"]["input-keyword1"].value
+    const keyword2 = document.forms["search_form"]["input-keyword2"].value
+    const keyword3 = document.forms["search_form"]["input-keyword3"].value
+
     if(form == ""){
         let error = document.getElementById("error-message-input");
         error.innerHTML = `<span style='color:#C22D39;'> Please enter search values </span>`;
@@ -14,5 +19,19 @@ CheckInput = () => {
         let error = document.getElementById("error-message-number");
         error.innerHTML = `<span style='color:#C22D39;'> Max number is 15</span>`;
         return false;
+    }
+
+    if(keyword1 != ""){
+        let usedKeyword1 = document.getElementById("used-keywords");
+        usedKeyword1.innerHTML = `<p>Nyckelord: ${keyword1}</p>`
+    }
+    if(keyword2 != ""){
+        let usedKeyword2 = document.getElementById("used-keywords");
+        usedKeyword1.innerHTML = `<p>Nyckelord: ${keyword2}</p>`
+    }
+    if(keyword2 != ""){
+        let usedKeyword3 = document.getElementById("used-keywords");
+        usedKeyword1.innerHTML = `<p>Nyckelord: ${keyword3}</p>`;
+        return true
     }
 }
