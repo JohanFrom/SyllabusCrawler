2b031c90bdbb9098008bb7c804ffe7c06058e593
JohanFrom <gouldsilver@gmail.com>
2022-03-11 13:03:33 +0100

    fixed js

--- static/index.js
+++ static/index.js
@@ -1,18 +1,19 @@
 // Check input values and loading 
+
 CheckInput = () => {
-    const form = document.forms["search_form"]["search-input"].value
-    const numbersInput = document.forms["search_form"]["amount-of-pages"].value
-    const keyword1 = document.forms["search_form"]["input-keyword1"].value;
-    const keyword2 = document.forms["search_form"]["input-keyword2"].value;
-    const keyword3 = document.forms["search_form"]["input-keyword3"].value;
+    let serachInput = document.forms["search_form"]["search-input"].value
+    let numbersInput = Number(document.forms["search_form"]["amount-of-pages"].value)
+    let keyword1 = document.forms["search_form"]["input-keyword1"].value;
+    let keyword2 = document.forms["search_form"]["input-keyword2"].value;
+    let keyword3 = document.forms["search_form"]["input-keyword3"].value;
     let displayKeywordInfo = document.getElementById("keywordInfo");
     let errorInput = document.getElementById("error-message-input");
     let errorNumber = document.getElementById("error-message-number");
 
-    if(form == ""){
+    if(serachInput == ""){
         errorInput.innerHTML = `<span style='color:#C22D39;'> Please enter search values </span>`;
         return false;
-    } else if(numbersInput == ""){
+    } else if(isNaN(numbersInput)){
         errorNumber.innerHTML = `<span style='color:#C22D39;'> Must be a number </span>`;
         return false;
     } else if( numbersInput > 15){
@@ -22,29 +23,31 @@ CheckInput = () => {
         displayKeywordInfo.innerHTML = `<span style='color:#C22D39;'>Du måste skriva in minst 1 nyckelord!</span>`;
         return false;
     }else{
+        console.log("HA");
         errorInput.innerHTML = `<span</span>`
         errorNumber.innerHTML = `<span</span>`
-        displayKeywordInfo.innerHTML = `<span> </span>`;
+        displayKeywordInfo.innerHTML = `<span></span>`;
         document.getElementById("loader").style.display = "block";
+        
         return true;
     }
 }
 
 // Check the scrape result
 CheckOutput = () => {
-    const errorMessage = document.getElementById("error-message");
     const pTag = document.getElementById("scrape-result");
     const keywordMessage = document.getElementById("keywordMessage");
-
-    keywordMessage.innerHTML = `<span>Nyckelord:</span>`;
+    const errorMessage = document.getElementById("error-message");
+    
+    errorMessage.innerHTML = `<span></span>`
 
     if(pTag == null || pTag == "") errorMessage.innerHTML = `<span>Fann inget resultat baserat på det som matats in!</span>`;
+    else keywordMessage.innerHTML = `<span>Nyckelord:</span>`;
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     if(document.URL == "http://127.0.0.1:5000/search") CheckOutput();
 })
-
 
 
 
