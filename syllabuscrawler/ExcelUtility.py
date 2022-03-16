37c7fa8504f33bb32173e3d25db3d8a92e9a778e
Bramalamm <fredrik.bramell@gmail.com>
2022-03-16 18:09:01 +0100

    blabla

--- syllabuscrawler/ExcelUtility.py
+++ syllabuscrawler/ExcelUtility.py
@@ -12,19 +12,14 @@ class ExcelUtility:
                     worksheet.write('A1', 'Länkar ↓ Nyckelord →')
                     for i, keyword in enumerate(keywords):
                         if len(keyword) == 0:
-                            #worksheet.write('A' + str(i+2), links[i])
-                            #worksheet.write('A' + str(1+2), "Inget nyckelord")
                             worksheet.write(string.ascii_uppercase[i+1] + str(1), 'Inget nyckelord')
                             worksheet.write('C' + str(i+2), r[i])
-                            #worksheet.write('C' + str(i+2), r[i])
-                            #worksheet.write(string.ascii_uppercase[i+1] + str(1), links[i])
+                            
                         else:
                             worksheet.write('A' + str(i+2), links[i])
                             worksheet.write(string.ascii_uppercase[i+1] + str(1), keywords[i])
                             worksheet.write('C' + str(i+2), r[i])
-                            #worksheet.write(string.ascii_uppercase[i+1] + str(1), links[i])
-                            #worksheet.write(string.ascii_uppercase[i] + str(1+i), r[0])
-
+                    
                     workbook.close()
                 except Exception as e:
                     print("")
